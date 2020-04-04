955cb9178fff36995b082e1353fc9fb5eed44c93
Nicolas Garcia <nicolasgarciafranco@gmail.com>
2020-04-03 21:36:29 -0500

    segunda-subida-calculadora

--- calculadora.js
+++ calculadora.js
@@ -1,16 +1,46 @@
+var resultadolimpio
 function numero(x) {
+    if (resultadolimpio == 'limpiar'){
+        var numero = document.querySelector("#resultado").innerHTML = "" + x;
+        resultadolimpio = 'limpio'
+    }else{
     var numero = document.querySelector("#resultado").innerHTML;
     var numeroTotal = numero + x;
     document.querySelector("#resultado").innerHTML = numeroTotal;
+    }
 }
 function operacion(x) {
     var numero = document.querySelector("#resultado").innerHTML;
-    var numeroTotal = numero + x;
-    document.querySelector("#resultado").innerHTML = numeroTotal;
+    var operacion = numero.split(" ");
+    var SoloUnOperador
+    switch (operacion[1]) {
+        case '+':
+            SoloUnOperador = true
+            break;
+        case '-':
+            SoloUnOperador = true
+            break;
+        case '/':
+            SoloUnOperador = true
+            break;
+        case '*':
+            SoloUnOperador = true
+            break;
+    }
+    if (SoloUnOperador === true){
+        document.querySelector("#resultado").innerHTML = numero;
+    }else{
+        var numeroTotal = numero + x;
+        document.querySelector("#resultado").innerHTML = numeroTotal;
+        resultadolimpio = 'limpio'
+    }
 }
 function resultado() {
     var numero = document.querySelector("#resultado").innerHTML;
     var operacion = numero.split(" ");
+    if (operacion[2] === undefined){
+        document.querySelector("#resultado").innerHTML = numero;
+    }else{
     var numero1 = Number(operacion[0])
     var numero2 = Number(operacion[2])
     var numeroFinal
@@ -29,4 +59,10 @@ function resultado() {
             break;
     }
     document.querySelector("#resultado").innerHTML = numeroFinal;
+    
+}
+    resultadolimpio = "limpiar";
+}
+function limpiar(){
+
 }
